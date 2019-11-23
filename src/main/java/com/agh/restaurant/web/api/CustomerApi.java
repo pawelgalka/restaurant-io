6c81e7d3f8e85331a3e9f3dff3fff09e2e53e1d4
Paweł <p.k.galka@gmail.com>
2019-11-23 14:24:22 +0100

    more customer api

--- src/main/java/com/agh/restaurant/web/api/CustomerApi.java
+++ src/main/java/com/agh/restaurant/web/api/CustomerApi.java
@@ -3,9 +3,9 @@ package com.agh.restaurant.web.api;
 import com.agh.restaurant.config.SecurityConfig;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -14,6 +14,16 @@ public class CustomerApi {
 
     @PostMapping(value = "/reserve")
     public HttpStatus createReservation(){
+        return HttpStatus.OK;
+    }
+
+    @DeleteMapping(value = "/reserve")
+    public HttpStatus deleteReservation(@RequestBody String customerName, @RequestBody Integer reservationId){
+        return HttpStatus.OK;
+    }
+
+    @PatchMapping(value = "/reserve")
+    public HttpStatus modifyReservation(@RequestBody String customerName, @RequestBody Integer reservationId, @RequestBody LocalDateTime newDate){
         return HttpStatus.OK;
     }
 }
